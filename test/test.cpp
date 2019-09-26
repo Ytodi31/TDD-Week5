1d4baf12dd66440ffe3f0fe32a21d0ebf334d182
YTodi31 <yashaarth@todi89.com>
2019-09-26 18:20:18 -0400

    Unit test documentation added

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,26 @@
+/**
+ * @copyright Copyright (c) Fall 2019 ENPM808X
+ *            This project is released under the MIT License
+ *
+ * @file test.cpp
+ * @brief PIDController class implementation file
+ *
+ * It contains the function definition to compute the new velocity of PID
+ *
+ * @author Yashaarth Todi [Driver]
+ * @author Raja Iskala [Navigator]
+ *
+ * @date 09-25-2019
+ */
+
 #include <gtest/gtest.h>
 
 #include "PIDController.h"
 
+
+/**
+ * @brief This test checks if controller has achieved set target
+ */
 TEST(PIDControllerTest, testPIDControllerComputeMethod) {
   PIDController controller;
   double velocity;
@@ -9,10 +28,12 @@ TEST(PIDControllerTest, testPIDControllerComputeMethod) {
   ASSERT_NEAR(15, velocity, 1);
 }
 
-
+/**
+ * @brief This test checks if PID Controller parameters are appropriate
+ */
 TEST (PIDTuningTest, testPIDParameters) {
   PIDController controller;
   ASSERT_EQ(0.5, controller.getKp());
   ASSERT_EQ(0.2, controller.getKd());
-  ASSERT_EQ(0.1, controller.getKd());
+  ASSERT_EQ(0.1, controller.getKi());
 }
